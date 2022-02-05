1a68031931eed9ac2e2da1e535e4b233000aab0d
Yoann Diep <yoann.diep@hotmail.fr>
2022-02-05 17:18:27 +0100

    added the cropping function

--- waste_net/utils.py
+++ waste_net/utils.py
@@ -2,6 +2,11 @@ import shutil
 import json
 import os
 import pandas as pd
+import tqdm
+from PIL import Image
+import numpy as np
+import cv2
+from pathlib import Path
 
 def copyFiles(input_folder, destination_folder, anns):
     '''Copy all files from the different batch directories and paste them in one folder with the proper ID'''
@@ -25,7 +30,7 @@ def copyFiles(input_folder, destination_folder, anns):
                     shutil.move(os.path.join(destination_folder, file),os.path.join(destination_folder, f"{img['id']}.jpg"))
 
 
-def create_taco_df(input_folder, anns_name):
+def create_taco_df(input_folder, anns_name, cropped_df):
 
     with open(os.path.join(input_folder, anns_name), 'r') as f:
         dataset = json.loads(f.read())
@@ -50,4 +55,57 @@ def create_taco_df(input_folder, anns_name):
         left_on="image_id",
         right_on="id").drop(columns="id")
 
+    df = df.merge(cropped_df, on ="ann_id")
+
     return df
+
+def cropping(df, input_folder, destination_folder):
+
+    l_img_paths = []
+    l_mask_paths = []
+
+    for i, row in tqdm(df.iterrows()):
+
+        # Skips rows with more than one segmentation polygon
+        if len(row["segmentation"]) == 1:
+
+            bbox = row['bbox']
+
+            # Opens the image
+            im = Image.open(os.path.join(input_folder, row['file_name']))
+
+            # Reshapes the segmentation polygon
+            points = np.array(row['segmentation']).reshape(
+            (-1, 2)).astype(np.int32)
+
+            # Creates the mask
+            mask = cv2.fillPoly(np.asarray(im) * 0, [points], (255, 255, 255))
+            mask = Image.fromarray(mask)
+
+            # Crops the image/mask in a square with 10% padding
+            delta = max((bbox[0] + bbox[2]), (bbox[1] + bbox[3]))
+            im = im.crop((bbox[0] - delta * 0.1, bbox[1] - delta * 0.1,
+                      delta * 1.1, delta * 1.1))
+            mask = mask.crop((bbox[0] - delta * 0.1, bbox[1] - delta * 0.1,
+                          delta * 1.1, delta * 1.1))
+
+            # Resizes the image/mask
+            cropped_resized_image = im.resize((128, 128))
+            cropped_resized_mask = mask.resize((128, 128))
+
+            # Saves the image/mask
+            cropped_resized_image.save(f'images/Cropped_image/{i}.jpeg')
+            cropped_resized_mask.save(f'images/Cropped_mask/{i}.jpg')
+
+        l_img_paths.append(f'{destination_folder}/Cropped_image/{i}.jpeg')
+        l_mask_paths.append(f'{destination_folder}/Cropped_mask/{i}.jpg')
+
+    cropped = pd.DataFrame({
+        "cropped_image": l_img_paths,
+        "cropped_mask": l_mask_paths
+    })
+
+    cropped["ann_id"] = cropped["cropped_image"].apply(
+        lambda x: int(Path(x).stem))
+
+    return cropped
