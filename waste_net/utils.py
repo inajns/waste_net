47364091cdedc7e725a4d4bd2e5f34931af77fab
Yoann Diep <yoann.diep@hotmail.fr>
2022-02-05 17:06:55 +0100

    Added the create_taco_df function

--- waste_net/utils.py
+++ waste_net/utils.py
@@ -1,24 +1,53 @@
 import shutil
 import json
 import os
+import pandas as pd
 
 def copyFiles(input_folder, destination_folder, anns):
     '''Copy all files from the different batch directories and paste them in one folder with the proper ID'''
 
     batches = []
 
-    for batch in input_folder:
+    for batch in os.listdir(input_folder):
         if "batch" in batch:
             batches.append(os.listdir(os.path.join(input_folder, batch)))
 
     with open(os.path.join(input_folder, anns), "r") as f:
         data = json.loads(f.read())
 
-    anns = data["annotations"]
+    imgs = data["images"]
 
     for idx, batch in enumerate(batches):
-        for img in batch:
-            for ann in anns:
-                if ann["file_name"] == f"batch_{idx+1}/{img}":
-                    shutil.copy(os.path.join(input_folder,f'batch_{idx+1}/{img}'), destination_folder)
-                    shutil.move(os.path.join(input_folder, img),os.path.join(destination_folder, f"{ann['id']}.jpg"))
+        for file in batch:
+            for img in imgs:
+                if img["file_name"] == f"batch_{idx+1}/{file}":
+                    shutil.copy(os.path.join(input_folder,f'batch_{idx+1}/{file}'), destination_folder)
+                    shutil.move(os.path.join(destination_folder, file),os.path.join(destination_folder, f"{img['id']}.jpg"))
+
+
+def create_taco_df(input_folder, anns_name):
+
+    with open(os.path.join(input_folder, anns_name), 'r') as f:
+        dataset = json.loads(f.read())
+
+    df = pd.DataFrame.from_dict(dataset["annotations"])
+    df.drop(columns=['area', 'iscrowd'], inplace=True)
+
+    categories = pd.DataFrame.from_dict(dataset["categories"])
+
+    df = df.merge(
+        categories,
+        left_on='category_id',
+        right_on='id').drop(columns="id_y").rename(columns={
+            "id_x": "ann_id",
+            "name": "category"
+        })
+
+    images = pd.DataFrame.from_dict(dataset["images"])
+
+    df = df.merge(
+        images[["id", "width", "height", "file_name"]],
+        left_on="image_id",
+        right_on="id").drop(columns="id")
+
+    return df
